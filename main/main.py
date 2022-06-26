4785218737162fcfde83b3d184086bf686ff3abc
noam melamed <noammelamed30@gmail.com>
2022-06-26 17:46:34 +0300

    filter words

--- main/main.py
+++ main/main.py
@@ -1,5 +1,7 @@
 import json
 import requests
+
+
 def process_text(text):
     headers = {'Content-Type': 'text/plain;charset=utf-8'}
     params = {
@@ -13,7 +15,6 @@ def process_text(text):
     }
     r = requests.post("https://nakdan-4-0.loadbalancer.dicta.org.il/addnikud", headers=headers, json=params)
     r.encoding = "UTF-8"
-    # return (r.text)
     x = json.loads(r.text)
     ds = []
     for item in x:
@@ -24,29 +25,41 @@ def process_text(text):
             y['morph'] = '0x{0:0{1}X}'.format(int(inner['morph']), 16)
         if len(y) > 0:
             ds.append(y)
-
     return ds
 
-def isFemale(item):
+
+def is_female(item):
     return item['morph'][12] == '4' or item['morph'][12] == '5'
 
 
-def isAdj(item):
+def is_adj(item):
     return item['morph'][13] == '1'
 
-def filterWords(items):
+
+def is_talking_about_noun(items, start, end):
+    for i in range(start, end):
+        if items[i]['morph'][13] == '6':
+            return True
+    return False
+
+
+def filter_words(items):
     out = []
     for i in range(len(items) - 1):
-        if isAdj(items[i]) and isFemale(items[i]):
+        j = i
+        if j <= 3:
+            j = 0
+        else:
+            j = j - 3
+        if is_adj(items[i]) and is_female(items[i]) and (not is_talking_about_noun(items, j, i-1)):
             out.append(items[i]['word'])
     return out
 
-# def speakingAboutNoun(items,i):
-#     for
 
-def main() :
-    proc = process_text("היא יפה מאוד ונמוכה אבל היא רוצה ללכת לים עם המזוודה הקטנה היפה שלה")
-    print(filterWords(proc))
+def main():
+    proc = process_text("היא היפה מאוד ונמוכה אבל היא רוצה ללכת לים עם המזוודה הקטנה היפה שלה")
+    print(filter_words(proc))
+
 
 if __name__ == '__main__':
     main()

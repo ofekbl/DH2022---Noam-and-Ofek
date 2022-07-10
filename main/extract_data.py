e4b2f853fc34e9f09d310c283b46b9161aec8d1a
noam melamed <noammelamed30@gmail.com>
2022-07-10 15:03:19 +0300

    years

--- main/extract_data.py
+++ main/extract_data.py
@@ -24,26 +24,19 @@ class Parsing:
                     lgs = root[1][0][0]
                     titles = root[0][0][0][0]
                     titles = titles.text.split('\n')
-                    # if i == 1:
-                    #     print(titles)
-                    #     for title in titles:
-                    #         print(title)
-                    # print(titless[0])
-
+                    artists = root[0][0][0][1].text.split('\n')
                     for lg in lgs:
                         for l in lg:
                             words = l.text.split(' ')
                             if is_hebrew_song(words):
-                                # print(words)
                                 hebrew_flag = True
                                 lyrics.append(l.text)
             except:
                 continue
-            # print(hebrew_flag)
             if hebrew_flag:
                 with codecs.open("../words_to_tag2.txt", "a", "utf8") as words_to_tag:
                     words_to_tag.write(titles[0] + "\n")
-            self.songs_lyrics.append(' '.join(lyrics))
+            self.songs_lyrics.append([titles[0],artists[0],' '.join(lyrics)])
 def is_hebrew_song(words):
     for word in words:
         if re.search('[a-zA-Z]', word) == None:  # we want only the Hebrew words
--- main/main.py
+++ main/main.py
@@ -1,22 +1,23 @@
 import extract_data
 import process_text
-# import Add_Year_To_Json
-# import songs_to_years
+import find_year_of_song
 
 
 def main():
     songs_lyrics = extract_data.parsing.songs_lyrics
-    print(songs_lyrics)
     filtered_songs = []
-    # for song in songs_lyrics:
-    #     print(song)
-    #     filter = process_text.process_text(song)
-    #     if len(filter) != 0:
-    #         filtered_songs.append(filter)
-    #     print(filtered_songs)
-    # print(Add_Year_To_Json)
-    # songs_to_years
-
+    i = -1
+    for song in songs_lyrics:
+        song_lyrics = song[2]
+        song_name = song[0]
+        song_artist = song[1]
+        print(song[0])
+        filter_words = process_text.process_text(song_lyrics)
+        if len(filter_words) != 0:
+            song_year = find_year_of_song.find_year(song_name,song_artist)
+            filtered_songs.append([song_name,song_year,filter_words])
+            i = i+1
+            print(filtered_songs[i])
 
 if __name__ == '__main__':
     main()
